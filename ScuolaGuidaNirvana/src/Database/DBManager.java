f67a9998a29eaa3506207315530fda1c8b18e71d
claudio <claudiopisa9884@gmail.com>
2022-06-04 14:35:09 +0200

    import classi

--- ScuolaGuidaNirvana/src/Database/DBManager.java
+++ ScuolaGuidaNirvana/src/Database/DBManager.java
@@ -1,5 +1,9 @@
 package Database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBManager
 {
     //TODO: Implementazione connessione al database H2
